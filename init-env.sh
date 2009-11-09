25773179a5e81109b479a1d07a8bf63320e1728d
Anthony PERARD <anthony.perard@gmail.com>
2009-11-09 17:43:22 +0100

    init-env.sh, Fix & Update script.
    	* init-env.sh: Here.

--- init-env.sh
+++ init-env.sh
@@ -1,7 +1,20 @@
 #! /bin/sh
 
-conf_dir=.myconf
+conf_dir="$HOME/.myconf"
+backup_dir="$conf_dir/backup"
 
-ln -fs $conf_dir/gitconfig ~/.gitconfig 
-ln -fs $conf_dir/dot.vimrc ~/.vimrc
-ln -fs $conf_dir/zshrc ~/.zshrc
+apply(){
+  if test -f "$HOME/$2" -a ! -L "$HOME/$2"; then
+    echo mkdir -p "$backup_dir"
+    echo mv "$1" "$backup_dir"
+  fi
+  echo ln -Tfs "$conf_dir/$1" "$HOME/$2"
+}
+
+apply gitconfig .gitconfig
+apply dot.vimrc .vimrc
+apply dot.vim  .vim
+apply zshrc .zshrc
+apply dot.screenrc .screenrc
+apply epita/dot.ion3 .ion3
+apply gostai/dot.Xdefaults .Xdefaults
