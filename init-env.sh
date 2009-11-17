2898128b2e752ee56e690cf76ddb6e76d9014629
Anthony PERARD <anthony.perard@gmail.com>
2009-11-18 00:19:10 +0100

    Add colorgcc.
    	* TODO: Updated.
    	* dot.colorgccrc: New, configuration file of colorgcc.
    	* init-env.sh: Add colorgcc and ask when a configuration file
    	  exist.
    	* script/colorgcc: New.

--- init-env.sh
+++ init-env.sh
@@ -5,10 +5,22 @@ backup_dir="$conf_dir/backup"
 
 apply(){
   if test -f "$HOME/$2" -a ! -L "$HOME/$2"; then
-    echo mkdir -p "$backup_dir"
-    echo mv "$1" "$backup_dir"
+    echo "~/$2 already exist, do you want to replace it ? [y/N]"
+    read answer
+    case answer in
+      y|Y)
+        mkdir -p "$backup_dir"
+        mv "$1" "$backup_dir"
+        echo "A backup of ~/$2 has placed in $backup_dir."
+        echo ln -Tfs "$conf_dir/$1" "$HOME/$2"
+        ;;
+      *)
+        return
+        ;;
+    esac
+  else
+    echo ln -Tfs "$conf_dir/$1" "$HOME/$2"
   fi
-  echo ln -Tfs "$conf_dir/$1" "$HOME/$2"
 }
 
 apply gitconfig .gitconfig
@@ -19,3 +31,4 @@ apply dot.screenrc .screenrc
 apply epita/dot.ion3 .ion3
 apply gostai/dot.Xdefaults .Xdefaults
 apply dot.Xmodmap .Xmodmap
+apply dot.colorgccrc .colorgccrc
