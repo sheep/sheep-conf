de7f959509fa7a68fd2500ce5245cfa90b3d27f4
Anthony PERARD <anthony.perard@gmail.com>
2009-11-24 16:37:55 +0100

    ion, Change bind for horizontal spliting.
    	* epita/dot.ion3/cfg_tiling.lua: Here.

--- epita/dot.ion3/cfg_tiling.lua
+++ epita/dot.ion3/cfg_tiling.lua
@@ -2,22 +2,23 @@
 -- Ion tiling module configuration file
 --
 
--- Bindings for the tilings. 
+-- Bindings for the tilings.
 
 defbindings("WTiling", {
     bdoc("Split current frame vertically."),
     kpress(META.."S", "WTiling.split_at(_, _sub, 'bottom', true)"),
-    
+
+    bdoc("Split current frame horizontally."),
+    kpress(META.."V", "WTiling.split_at(_, _sub, 'right', true)"),
+
     bdoc("Go to frame above/below/right/left of current frame."),
     kpress(META.."P", "ioncore.goto_next(_sub, 'up', {no_ascend=_})"),
     kpress(META.."N", "ioncore.goto_next(_sub, 'down', {no_ascend=_})"),
     kpress(META.."Tab", "ioncore.goto_next(_sub, 'right')"),
+
     submap(META.."K", {
         kpress("Tab", "ioncore.goto_next(_sub, 'left')"),
-        
-        bdoc("Split current frame horizontally."),
-        kpress("S", "WTiling.split_at(_, _sub, 'right', true)"),
-        
+
         bdoc("Destroy current frame."),
         kpress("X", "WTiling.unsplit_at(_, _sub)"),
     }),
@@ -37,23 +38,23 @@ defbindings("WFrame.floating", {
 -- Context menu for tiled workspaces.
 
 defctxmenu("WTiling", "Tiling", {
-    menuentry("Destroy frame", 
+    menuentry("Destroy frame",
               "WTiling.unsplit_at(_, _sub)"),
 
-    menuentry("Split vertically", 
+    menuentry("Split vertically",
               "WTiling.split_at(_, _sub, 'bottom', true)"),
-    menuentry("Split horizontally", 
+    menuentry("Split horizontally",
               "WTiling.split_at(_, _sub, 'right', true)"),
-    
+
     menuentry("Flip", "WTiling.flip_at(_, _sub)"),
     menuentry("Transpose", "WTiling.transpose_at(_, _sub)"),
-    
+
     menuentry("Untile", "mod_tiling.untile(_)"),
-    
+
     submenu("Float split", {
-        menuentry("At left", 
+        menuentry("At left",
                   "WTiling.set_floating_at(_, _sub, 'toggle', 'left')"),
-        menuentry("At right", 
+        menuentry("At right",
                   "WTiling.set_floating_at(_, _sub, 'toggle', 'right')"),
         menuentry("Above",
                   "WTiling.set_floating_at(_, _sub, 'toggle', 'up')"),
@@ -62,9 +63,9 @@ defctxmenu("WTiling", "Tiling", {
     }),
 
     submenu("At root", {
-        menuentry("Split vertically", 
+        menuentry("Split vertically",
                   "WTiling.split_top(_, 'bottom')"),
-        menuentry("Split horizontally", 
+        menuentry("Split horizontally",
                   "WTiling.split_top(_, 'right')"),
         menuentry("Flip", "WTiling.flip_at(_)"),
         menuentry("Transpose", "WTiling.transpose_at(_)"),
@@ -72,7 +73,7 @@ defctxmenu("WTiling", "Tiling", {
 })
 
 
--- Extra context menu extra entries for floatframes. 
+-- Extra context menu extra entries for floatframes.
 
 defctxmenu("WFrame.floating", "Floating frame", {
     append=true,
