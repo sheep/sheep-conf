4b93bb999a140007283adbe7e2c3b7281aafef8b
Anthony PERARD <anthony.perard@gmail.com>
2010-04-01 12:44:55 +0200

    ion3, Add a hook for flash fullscreen (doesn't work without this).
    	* dot.ion3/cfg_kludges.lua: Here.

--- dot.ion3/cfg_kludges.lua
+++ dot.ion3/cfg_kludges.lua
@@ -105,6 +105,61 @@ defwinprop {
   target = "browserframe",
 }
 
+-- Flash player opens full-screen windows from Firefox with size
+-- request 200x200.  It closes them if they lose focus, but also if
+-- they get focus too quickly.  Therefore set focus 200 ms after
+-- such a window is mapped (this may need to be adjusted on slower
+-- computers).
+
+_NET_WM_STATE = ioncore.x_intern_atom('_NET_WM_STATE', false)
+_NET_WM_STATE_FULLSCREEN = ioncore.x_intern_atom('_NET_WM_STATE_FULLSCREEN', false)
+
+function is_fullscreen(cwin)
+  local state = ioncore.x_get_window_property(cwin:xid(), _NET_WM_STATE, 4, 1, true)
+  if state then
+    for k, v in pairs(state) do
+      if v == _NET_WM_STATE_FULLSCREEN then
+        return true
+      end
+    end
+  end
+  return false
+end
+
+defwinprop {
+  class = "Firefox",
+  match = function(prop, cwin, id)
+    local geom = cwin:geom()
+    return is_fullscreen(cwin) -- and geom.w == 200 and geom.h == 200
+  end,
+  switchto = false,
+  flash_fullscreen = true,
+}
+
+defwinprop {
+  -- match for google-chrome flash full screen
+  class = "Exe",
+  match = function(prop, cwin, id)
+    local geom = cwin:geom()
+    return is_fullscreen(cwin) -- and geom.w == 200 and geom.h == 200
+  end,
+  switchto = false,
+  flash_fullscreen = true,
+}
+
+ioncore.get_hook('clientwin_do_manage_alt'):add(
+function(cwin, table)
+  local winprop = ioncore.getwinprop(cwin)
+  if winprop and winprop.flash_fullscreen then
+    local timer = ioncore.create_timer()
+    timer:set(100, function() cwin:goto() end)
+    return true
+  else
+    return false
+  end
+end
+)
+
 -- Define some additional title shortening rules to use when the full
 -- title doesn't fit in the available space. The first-defined matching 
 -- rule that succeeds in making the title short enough is used.
