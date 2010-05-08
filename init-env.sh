f6e57f3969396c7c35981df1cdf53624640222f7
Anthony PERARD <anthony.perard@gmail.com>
2010-05-08 16:46:56 +0200

    Fix and update init-env.sh.
    	* init-env.sh: Here.

--- init-env.sh
+++ init-env.sh
@@ -3,23 +3,34 @@
 conf_dir="$HOME/.myconf"
 backup_dir="$conf_dir/backup"
 
+# command
+ln="ln --no-target-directory --force --symbolic"
+
+debug(){
+  echo "[DEBUG] $*"
+}
+
+info(){
+  echo "[INFO] $*"
+}
+
 apply(){
-  if test -f "$HOME/$2" -a ! -L "$HOME/$2"; then
-    echo "~/$2 already exist, do you want to replace it ? [y/N]"
+  if test -e "$HOME/$2" -a ! -L "$HOME/$2"; then
+    echo -n "~/$2 already exist, do you want to replace it ? [y/N] "
     read answer
-    case answer in
+    case "$answer" in
       y|Y)
         mkdir -p "$backup_dir"
-        mv "$1" "$backup_dir"
+        mv "$HOME/$2" "$backup_dir"
         echo "A backup of ~/$2 has placed in $backup_dir."
-        echo ln -Tfs "$conf_dir/$1" "$HOME/$2"
+        $ln "$conf_dir/$1" "$HOME/$2"
         ;;
       *)
         return
         ;;
     esac
   else
-    echo ln -Tfs "$conf_dir/$1" "$HOME/$2"
+    $ln "$conf_dir/$1" "$HOME/$2"
   fi
 }
 
@@ -31,4 +42,13 @@ apply dot.screenrc .screenrc
 apply dot.ion3 .ion3
 apply gostai/dot.Xdefaults .Xdefaults
 apply dot.Xmodmap .Xmodmap
+apply xinitrc .xinitrc
+
+# Setup colorgcc
+info "Setup of colorgcc"
 apply dot.colorgccrc .colorgccrc
+bin_dir="$HOME/bin"
+mkdir -p "$bin_dir"
+$ln "$conf_dir/script/colorgcc" "$bin_dir/gcc"
+$ln "$conf_dir/script/colorgcc" "$bin_dir/g++"
+$ln "$conf_dir/script/colorgcc" "$bin_dir/cc"
