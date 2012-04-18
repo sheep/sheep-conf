102e2812e4d28d2fadf64271dfb392d023331ddb
Anthony PERARD <anthony.perard@citrix.com>
2012-04-18 11:46:24 +0100

    wmii, plugin keyboard: Change wmii key binding will switch keymaping.

--- wmii/plugins/keyboard.py
+++ wmii/plugins/keyboard.py
@@ -18,9 +18,15 @@ def load_layout(layout, name=None, xmodmap=None):
     monitor.refresh()
 
 def us_keyboard():
+    # Change binding to default
+    keys.defs['web_tag'] = 'equal'
+    setattr(keys, 'mode', 'main')
     load_layout(('us', ''), 'us', '~/.Xmodmap')
 
 def dvorak_programmer_keyboard():
+    # Change binding to avoid key conflict (between = and 9)
+    keys.defs['web_tag'] = 'numbersign' # '#'
+    setattr(keys, 'mode', 'main')
     load_layout(('-layout', 'us', '-variant', 'dvp',
             '-option', 'compose:102', '-option', 'numpad:shift3',
             '-option', 'kpdl:semi', '-option', 'keypad:atm',
@@ -29,6 +35,9 @@ def dvorak_programmer_keyboard():
         None)
 
 def dvorak_keyboard():
+    # Change binding to default
+    keys.defs['web_tag'] = 'equal'
+    setattr(keys, 'mode', 'main')
     load_layout(('dvorak', ''), 'dv', None)
 
 def update(self):
